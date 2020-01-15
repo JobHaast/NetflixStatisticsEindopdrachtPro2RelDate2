3ae8a8d2bf57f2ca5182e67ebc361397f86a372c
NoahK <ns.korten@student.avans.nl>
2020-01-15 20:46:20 +0100

    Groot deel van de tests van Checks is af, er moeten er nog een paar toegevoegd worden

--- NetflixStatisticsEindopdrachtPro2&RelDate2/src/logic/Checks.java
+++ NetflixStatisticsEindopdrachtPro2&RelDate2/src/logic/Checks.java
@@ -56,5 +56,4 @@ public class Checks {
         }
         return check;
     }
-
 }
---
+++ Tests/logic/ChecksTest.java
@@ -0,0 +1,142 @@
+package logic;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChecksTest {
+
+    @Test
+    void testcheckIfNotNullWithInputNullShouldReturnFalse() {
+        //Arrange
+        String testString = null;
+
+        //Act
+        boolean testCheck = Checks.checkIfNotNull(testString);
+
+        //Assert
+        Assertions.assertFalse(testCheck);
+    }
+
+    @Test
+    void testcheckIfNotNullWithInputtestShouldReturnTrue() {
+        //Arrange
+        String testString = "test";
+
+        //Act
+        boolean testCheck = Checks.checkIfNotNull(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfNotNullWithInputEmptyStringShouldReturnTrue() {
+        //Arrange
+        String testString = "";
+
+        //Act
+        boolean testCheck = Checks.checkIfNotNull(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfLettersOnlyWithInputtestShouldReturnTrue() {
+        //Arrange
+        String testString = "test";
+
+        //Act
+        boolean testCheck = Checks.checkIfLettersOnly(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfLettersOnlyWithInputtestandAllowedSpecialCharactersAndSpacesShouldReturnTrue() {
+        //Arrange
+        String testString = "test- \'. ";
+
+        //Act
+        boolean testCheck = Checks.checkIfLettersOnly(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfLettersOnlyWithInput12345ShouldReturnFalse() {
+        //Arrange
+        String testString = "12345";
+
+        //Act
+        boolean testCheck = Checks.checkIfLettersOnly(testString);
+
+        //Assert
+        Assertions.assertFalse(testCheck);
+    }
+
+    @Test
+    void testcheckIfLettersOnlyWithInputEmptyStringShouldReturnTrue() {
+        //Arrange
+        String testString = "";
+
+        //Act
+        boolean testCheck = Checks.checkIfLettersOnly(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfNumbersOnlyWithInput1234ShouldReturnTrue() {
+        //Arrange
+        String testString = "1234";
+
+        //Act
+        boolean testCheck = Checks.checkIfNumbersOnly(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfNumbersOnlyWithInput123_4ShouldReturnTrue() {
+        //Arrange
+        String testString = "123 4";
+
+        //Act
+        boolean testCheck = Checks.checkIfNumbersOnly(testString);
+
+        //Assert
+        Assertions.assertTrue(testCheck);
+    }
+
+    @Test
+    void testcheckIfNumbersOnlyWithInput123_4abcShouldReturnFalse() {
+        //Arrange
+        String testString = "123 4abc";
+
+        //Act
+        boolean testCheck = Checks.checkIfNumbersOnly(testString);
+
+        //Assert
+        Assertions.assertFalse(testCheck);
+    }
+
+    @Test
+    void testcheckIfNumbersOnlyWithInputabcShouldReturnFalse() {
+        //Arrange
+        String testString = "abc";
+
+        //Act
+        boolean testCheck = Checks.checkIfNumbersOnly(testString);
+
+        //Assert
+        Assertions.assertFalse(testCheck);
+    }
+
+}
