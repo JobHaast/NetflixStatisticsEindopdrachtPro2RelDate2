232e9572c795c4dc36f04177e4d6d536341c791f
Job Haast <jobhaast@hotmail.com>
2020-01-19 19:58:34 +0100

    Finishing touch (2)

--- NetflixStatisticsEindopdrachtPro2&RelDate2/src/database/Update.java
+++ NetflixStatisticsEindopdrachtPro2&RelDate2/src/database/Update.java
@@ -21,7 +21,7 @@ public class Update {
     }
 
     //Method for updating an account
-    public String updateAccount(String accountName, String email, String phoneNumber, String password){
+    public String updateAccount(String accountName, String email, String phoneNumber, String password) {
         try {
             // Import the downloaded driver.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -30,15 +30,13 @@ public class Update {
             statement = con.createStatement();
             // Execute the query
             statement.executeUpdate("UPDATE Account \n" +
-                    "SET Email = '"+email+"', PhoneNumber = '"+phoneNumber+"', Password = '"+password+"' WHERE AccountName = '"+accountName+"';");
+                    "SET Email = '" + email + "', PhoneNumber = '" + phoneNumber + "', Password = '" + password + "' WHERE AccountName = '" + accountName + "';");
+//            This query updates an account in the table called Account. Email is replaced by the variable email and Phonenumber is replaced by the variable Phonenumber where the Account name corresponds with the variable accountName.
 
 //            Handle any errors that may have occurred.
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        finally {
+        } finally {
             if (resultSet != null) try {
                 resultSet.close();
             } catch (Exception e) {
@@ -55,30 +53,25 @@ public class Update {
         return "Account updated";
     }
 
-    //Method for updating an address
-    public String updateAddress(String city, String streetName, int number, String addition, Read read){
+    //Method for updating an address (currently not being used)
+    public String updateAddress(String city, String streetName, int number, String addition, Read read) {
         try {
             // Import the downloaded driver.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             // Make a connection with the database
             con = DriverManager.getConnection(connectionUrl);
             statement = con.createStatement();
-            if(read.getAddresses().size() == 0){
+            if (read.getAddresses().size() == 0) {
                 // Execute the query
-                statement.executeUpdate("UPDATE Account \n" +
-                        "SET Email = 'emai.hotmail.com', PhoneNumber = '0690675432', Password = 'noah', AddressId = '1'\n" +
-                        "WHERE AccountName = 'Noah';");
-            }else{
+                statement.executeUpdate("");
+            } else {
                 throw new Exception("Error");
             }
 
 //            Handle any errors that may have occurred.
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        finally {
+        } finally {
             if (resultSet != null) try {
                 resultSet.close();
             } catch (Exception e) {
@@ -96,25 +89,25 @@ public class Update {
     }
 
     //Method for updating a profile
-    public String updateProfile(String accountName, String profileName, String profileLanguage, String birthDay){
+    public String updateProfile(String accountName, String profileName, String profileLanguage, String birthDay) {
         try {
             // Import the downloaded driver.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
             // Make a connection with the database
             con = DriverManager.getConnection(connectionUrl);
             statement = con.createStatement();
-                // Execute the query
+            // Execute the query
             statement.executeUpdate("UPDATE Profile \n" +
-                        "SET ProfileLanguage = '"+profileLanguage+"', Birthday = '"+birthDay+"'\n" +
-                        "WHERE AccountName = '"+accountName+"' AND ProfileName = '"+profileName+"';");
+                    "SET ProfileLanguage = '" + profileLanguage + "', Birthday = '" + birthDay + "'\n" +
+                    "WHERE AccountName = '" + accountName + "' AND ProfileName = '" + profileName + "';");
+//            This query updates a profile in the table called Profile. ProfileLanguage is replaced by the variable profileLanguage and BirthDay is replaced by the variable birthDay
+//            where the Account name corresponds with the variable accountName and ProfileName corresponds with the variable profileName.
+
 
 //            Handle any errors that may have occurred.
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        finally {
+        } finally {
             if (resultSet != null) try {
                 resultSet.close();
             } catch (Exception e) {
@@ -132,7 +125,7 @@ public class Update {
     }
 
     //Method for updating a watched program
-    public String updateWatchedProgram(String accountName, String profileName, int programId, int percentageWatched){
+    public String updateWatchedProgram(String accountName, String profileName, int programId, int percentageWatched) {
         try {
             // Import the downloaded driver.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -141,16 +134,16 @@ public class Update {
             statement = con.createStatement();
             // Execute the query
             statement.executeUpdate("UPDATE Profile_Program \n" +
-                    "SET PercentageWatched = '"+percentageWatched+"'\n" +
-                    "WHERE AccountName = '"+accountName+"' AND ProfileName = '"+profileName+"' AND ProgramId = "+programId+";");
+                    "SET PercentageWatched = '" + percentageWatched + "'\n" +
+                    "WHERE AccountName = '" + accountName + "' AND ProfileName = '" + profileName + "' AND ProgramId = " + programId + ";");
+//            This query updates a watched program in the table called Profile_WatchedProgram. PercentageWatched is replaced by the variable percentageWatched where the Accountname corresponds with the variable accountName,
+//            the ProfileName corresponds with the variable profileName and Programid corresponds with the variable programId.
+
 
 //            Handle any errors that may have occurred.
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        finally {
+        } finally {
             if (resultSet != null) try {
                 resultSet.close();
             } catch (Exception e) {
@@ -168,7 +161,7 @@ public class Update {
     }
 
     //Method for updating an administrator
-    public String updateAdministrator(String accountName, String passWord){
+    public String updateAdministrator(String accountName, String passWord) {
         try {
             // Import the downloaded driver.
             Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
@@ -177,16 +170,15 @@ public class Update {
             statement = con.createStatement();
             // Execute the query
             statement.executeUpdate("UPDATE Account\n" +
-                    "SET Password = '"+passWord+"'\n" +
-                    "WHERE AccountName = '"+accountName+"';");
+                    "SET Password = '" + passWord + "'\n" +
+                    "WHERE AccountName = '" + accountName + "';");
+//            This query updates an account in the table called Account. Password is replaced by the variable passWord where the Account name corresponds with the variable accountName.
+
 
 //            Handle any errors that may have occurred.
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
-        }
-
-        finally {
+        } finally {
             if (resultSet != null) try {
                 resultSet.close();
             } catch (Exception e) {
--- NetflixStatisticsEindopdrachtPro2&RelDate2/src/guiscenes/crudactions/CreateAccount.java
+++ NetflixStatisticsEindopdrachtPro2&RelDate2/src/guiscenes/crudactions/CreateAccount.java
@@ -271,7 +271,7 @@ public class CreateAccount {
         Color backgroundColorUnder = Color.web("rgb(77, 73, 73)");
         mainScene.backgroundProperty().set(new Background(new BackgroundFill(backgroundColorUnder, CornerRadii.EMPTY, Insets.EMPTY)));
 
-        Scene scene = new Scene(mainScene, screenSize.getWidth(), screenSize.getHeight()*0.978);
+        Scene scene = new Scene(mainScene, screenSize.getWidth(), screenSize.getHeight() * 0.978);
         return scene;
     }
 }
